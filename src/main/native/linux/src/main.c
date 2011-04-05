3c69ad74e537174ad9a833d9b9ae8fa485c132d6
Josh Long <josh.long@springsource.com>
2011-04-05 02:01:54 -0700

    cleanup of the project.

--- src/main/native/linux/src/main.c
+++ src/main/native/linux/src/main.c
@@ -1,19 +1,19 @@
 /** 
-  Initial work on my attempts to provide an x-platform way to expose the operating system as a client to your application. 
-  
-  The abstraction should permit: 
-   1) letting java code drive a view (determine which files are shown, and perhaps expand the data by adding columns) in explorer/nautilus/finder window:
-    1.a.) show files with badges/icons
-    1.b.) show folders with badges/icons 
-    1.c.) show custom file metadata/properties (this changes per OS - on Gnome this is a property page)
-    1.d.) show custom columns
-    1.e.) show specialized sidebars (e.g., depending on what's selected...) 
-   2) letting java code be triggered when an interesting action happens to the file (this might be less important as Swing Desktop and Adobe AIR already let you do this)
-   3) monitor folders for certain types of server side events (this should already be possible with the Spring Integration module)  
+ Initial work on my attempts to provide an x-platform way to expose the operating system as a client to your application.
 
-http://taschenorakel.de/svn/repos/bulldozer/trunk/documentation/NautilusExtensions.html 
+ The abstraction should permit:
+ 1) letting java code drive a view (determine which files are shown, and perhaps expand the data by adding columns) in explorer/nautilus/finder window:
+ 1.a.) show files with badges/icons
+ 1.b.) show folders with badges/icons
+ 1.c.) show custom file metadata/properties (this changes per OS - on Gnome this is a property page)
+ 1.d.) show custom columns
+ 1.e.) show specialized sidebars (e.g., depending on what's selected...)
+ 2) letting java code be triggered when an interesting action happens to the file (this might be less important as Swing Desktop and Adobe AIR already let you do this)
+ 3) monitor folders for certain types of server side events (this should already be possible with the Spring Integration module)
+
+ http://taschenorakel.de/svn/repos/bulldozer/trunk/documentation/NautilusExtensions.html
  
-*/
+ */
 
 #define GETTEXT_PACKAGE "gtk-hello"
 #define LOCALEDIR "mo"
@@ -25,114 +25,92 @@ http://taschenorakel.de/svn/repos/bulldozer/trunk/documentation/NautilusExtensio
 #include <libnautilus-extension/nautilus-info-provider.h>
 #include <libnautilus-extension/nautilus-menu-provider.h>
 #include <libnautilus-extension/nautilus-property-page-provider.h>
-
 #include <libgnomevfs/gnome-vfs-utils.h>
- 
-
-//// todo what happened to this? apparently it was deprecated ? http://opensolaris.org/jive/thread.jspa?threadID=128265 
-//// #include <eel/eel-vfs-extensions.h> 
-
 #include <glib/gi18n-lib.h>
-//#include <glib/glib.h>
-
-
 #include <gtk/gtktable.h>
 #include <gtk/gtkvbox.h>
 #include <gtk/gtkhbox.h>
 #include <gtk/gtklabel.h>
 #include <gtk/gtk.h>
 #include <gtk/gtkentry.h>
-
-/// #include <glade/glade.h> 
-
 #include <string.h>
 #include <time.h>
-
 #include <unistd.h>
 #include <stdlib.h> 
 
+/*
+
+ // my implementation specific types
+ static GType foo_extension_type;
 
 
-// utility functions 
-
-void note(char * msg){
-  printf(msg, ""); fflush(stdout);
-}
+ static void
+ foo_extension_register_type (GTypeModule *module)
+ {
+ static const GTypeInfo info = {
+ sizeof (FooExtensionClass),
+ (GBaseInitFunc) NULL,
+ (GBaseFinalizeFunc) NULL,
+ (GClassInitFunc) foo_extension_class_init,
+ NULL,
+ NULL,
+ sizeof (FooExtension),
+ 0,
+ (GInstanceInitFunc) foo_extension_instance_init,
+ };
  
-// interface contract to be a nautilus extension provider 
-
-  /*
-
-// my implementation specific types
-static GType foo_extension_type;
-
-
-static void
-foo_extension_register_type (GTypeModule *module)
-{
-        static const GTypeInfo info = {
-                sizeof (FooExtensionClass),
-                (GBaseInitFunc) NULL,
-                (GBaseFinalizeFunc) NULL,
-                (GClassInitFunc) foo_extension_class_init,
-                NULL,
-                NULL,
-                sizeof (FooExtension),
-                0,
-                (GInstanceInitFunc) foo_extension_instance_init,
-        };
- 
-	foo_extension_type = g_type_module_register_type (module,
-							  G_TYPE_OBJECT,
-							  "FooExtension",
-							  &info, 0);
+ foo_extension_type = g_type_module_register_type (module,
+ G_TYPE_OBJECT,
+ "FooExtension",
+ &info, 0);
 
  
-}
+ }
 
-GType
-foo_extension_get_type (void)
-{
-	return foo_extension_type;
-}
-
-
-
-static GType provider_types[1];
-
-void
-nautilus_module_initialize (GTypeModule  *module) 
-{
-	foo_extension_register_type (module);
-
-	provider_types[0] = foo_extension_get_type ();
-}
-
-void 
-nautilus_module_shutdown (void)
-{ 
-}
+ GType
+ foo_extension_get_type (void)
+ {
+ return foo_extension_type;
+ }
 
 
 
-void
-nautilus_module_list_types (const GType **types,
-			    int *num_types)
-{
+ static GType provider_types[1];
+
+ void
+ nautilus_module_initialize (GTypeModule  *module)
+ {
+ foo_extension_register_type (module);
+
+ provider_types[0] = foo_extension_get_type ();
+ }
+
+ void
+ nautilus_module_shutdown (void)
+ {
+ }
 
 
 
-	*types = provider_types;
-	*num_types = G_N_ELEMENTS (provider_types);
-}
+ void
+ nautilus_module_list_types (const GType **types,
+ int *num_types)
+ {
+
+
+
+ *types = provider_types;
+ *num_types = G_N_ELEMENTS (provider_types);
+ }
 
  */
- 
-int main () {  
+void note(char * msg) {
+	g_print(msg);
+	g_print("\n");
+	fflush(stdout);
+}
+int main() {
 
- note( "hello, world! " ) ; 	
- 
- g_print ("Initializing nautilus-share extension\n"); 
-} 
-
+	note("hello, world x3 -- Initializing nautilus-share extension");
+}
 
